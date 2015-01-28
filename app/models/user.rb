fb3aebec595b689b368a24499e519308da0efbb2
David Rifkin <davidlrifkin@gmail.com>
2015-01-28 10:49:28 -0500

    Add initial relations and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :orders
+  has_many :pickups
+
+  validates :user_name, presence: true
+  validates :email, presence: true
+
   # Remember to create a migration!
 end
