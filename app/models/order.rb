e186972eddd8391e3892949dbf73a4184e3772c0
David Rifkin <davidlrifkin@gmail.com>
2015-01-28 17:08:12 -0500

    Add pickup and unclaimed method to Order class

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,14 @@
 class Order < ActiveRecord::Base
   belongs_to :requestor, class_name: "User"
   belongs_to :item
+  has_one :pickup
 
   def restaurant
     self.item.restaurant
+  end
+
+  def self.unclaimed
+    Order.select {|o| !o.pickup}
   end
 
   validates :requestor_id, :item_id, :presence => true
