3645539c73b5991dccaac777217223c063b7d345
David Rifkin <davidlrifkin@gmail.com>
2015-01-28 18:13:40 -0500

    Temporary commit to reopen master on a new, non-broken computer

---
+++ app/controllers/pickup.rb
@@ -0,0 +1,16 @@
+get 'pickup/:id' do |id|
+  @pickups = Pickup.where(retriever_id: id)
+  erb :'/pickup/all'
+end
+
+post '/pickup' do
+  pickup = Pickup.new(retriever_id: current_user.id, order_id: params[:order])
+
+  if pickup.save
+    redirect '/'
+  else
+    # errors
+    redirect '/order/search'
+  end
+end
+
