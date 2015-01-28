9c165c31a6409fbd461b467c8ef98ceaec6b4442
David Rifkin <davidlrifkin@gmail.com>
2015-01-28 12:17:13 -0500

    Add Post route handlers for login and signup

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -3,11 +3,34 @@ get '/' do
 end
 
 get '/login' do
-  erb ':auth/_login'
+  erb :'/auth/_login'
+end
+
+post '/login' do
+  user = User.find_by(user_name: params[:user][:user_name])
+
+  if user && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/'
+  end
+  redirect '/login'
 end
 
 get '/signup' do
-  erb ':auth/_signup'
+  erb :'/auth/_signup'
+end
+
+post '/signup' do
+  user = User.new(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    #errors
+    redirect '/signup'
+  end
+
 end
 
 get '/logout' do
