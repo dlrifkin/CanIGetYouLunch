5cdca77654a1f78397a214892611c78bb04e6ee3
David Rifkin <davidlrifkin@gmail.com>
2015-01-28 11:55:05 -0500

    Add Get routes for user auth

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,16 @@
 get '/' do
-  erb :welcome
+  erb :index
 end
 
 get '/login' do
-  
+  erb ':auth/_login'
 end
 
 get '/signup' do
-  
+  erb ':auth/_signup'
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
