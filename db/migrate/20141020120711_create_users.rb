014430073383e7c6d92aef5cce9191d7649322c7
David Rifkin <davidlrifkin@gmail.com>
2015-01-28 10:39:57 -0500

    Create migrations

--- db/migrate/20141020120711_create_users.rb
+++ db/migrate/20141020120711_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :user_name
       t.string :password_digest
+      t.string :name
+      t.string :email
     end
   end
 end
