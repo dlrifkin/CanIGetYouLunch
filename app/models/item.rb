0b032a713b1a9295107ea923f12cf8fbf7ffd9f2
David Rifkin <davidlrifkin@gmail.com>
2015-01-28 19:13:22 -0500

    Add divs to order views

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,6 @@ class Item < ActiveRecord::Base
   has_many :orders
   has_many :pickups
 
-  validates :name, :category, :restaurant_id, presence: true
+  validates :name, :restaurant_id, presence: true
 
 end
